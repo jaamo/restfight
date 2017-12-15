3aeeb90dde9ade3b9ff26912bffe9a99a02f9047
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2017-12-15 21:47:31 +0200

    A little refactor.

--- restfight/main.go
+++ restfight/main.go
@@ -8,67 +8,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// The person Type (more like an object)
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
+// Status struct.
+type Status struct {
+	gameId string `json:"id,omitempty"`,
+	turn   string `json:"turn,omitempty"`
 }
 
-var people []Person
-
-// Display all from the people var
-func GetPeople(w http.ResponseWriter, r *http.Request) {
+/**
+ * Return game status.
+ */
+func getStatus(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
-// Display a single data
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Person{})
-}
-
-// create a new item
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var person Person
-	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-
-// Delete an item
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(people)
-	}
-}
-
-// main function to boot up everything
+/**
+ * Main function.
+ */
 func main() {
+
+	// Create router,
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	router.HandleFunc("/people", GetPeople).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+
+	// Register routes.
+	router.HandleFunc("/people", getStatus).Methods("GET")
+	// router.HandleFunc("/people/{id}", fn).Methods("POST")
+
+	// Start the server.
 	log.Fatal(http.ListenAndServe(":8000", router))
+
 }
