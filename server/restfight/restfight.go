9e1867448c1043efaecbdd7309a0b2aa2d432602
Jaakko Alajoki <jaamo@Jaakkos-MacBook-Pro.local>
2017-12-22 22:28:30 +0200

    Desperate try to move things forward.

--- server/restfight/restfight.go
+++ server/restfight/restfight.go
@@ -6,6 +6,36 @@ type Status struct {
 	Turn   string `json:"turn,omitempty"`
 }
 
+/**
+ * Size of the arena.
+ */
+const ArenaSize = 100
+
+/**
+ * Arena type. Empty.
+ */
+const ArenaTypeEmpty = 0
+
+/**
+ * Single cell object.
+ */
+type Cell struct {
+	Type  int8   `json:"type,omitempty"`
+	Robot string `json:"robot,omitempty"`
+}
+
+/**
+ * Get game status.
+ */
 func GetStatus() int {
+
+	var arena [ArenaSize][ArenaSize]Cell
+
+	for x := 0; x < ArenaSize; x++ {
+		for y := 0; y < ArenaSize; y++ {
+			arena[x][y] = Cell{Type: ArenaTypeEmpty}
+		}
+	}
+
 	return 2
 }
